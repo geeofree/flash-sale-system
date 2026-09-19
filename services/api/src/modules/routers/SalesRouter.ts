7b7168e96ccadd5605465ccffc22ec82a3c9f258
Geo Galagaran <17492631+geeofree@users.noreply.github.com>
2026-09-19 09:32:26 +0800

    fix: update sales endpoint semantics

--- services/api/src/modules/routers/SalesRouter.ts
+++ services/api/src/modules/routers/SalesRouter.ts
@@ -4,15 +4,21 @@ import { SalesService } from '../services/SalesService.js';
 
 export const SalesRouter = Router();
 
-SalesRouter.get('/status', async (_req, res) => {
+SalesRouter.get('/', async (_req, res) => {
   const salesService = DIContainer.get(SalesService);
-  const saleStatus = await salesService.getSaleStatus();
+  const saleStatus = await salesService.getAllSales();
+  res.json(saleStatus.result).status(saleStatus.statusCode);
+});
+
+SalesRouter.get('/latest', async (_req, res) => {
+  const salesService = DIContainer.get(SalesService);
+  const saleStatus = await salesService.getLatestSale();
   res.json(saleStatus.result).status(saleStatus.statusCode);
 });
 
 SalesRouter.post('/', async (req, res) => {
   const params = req.body;
   const salesService = DIContainer.get(SalesService);
-  const response = await salesService.createFlashSale(params);
+  const response = await salesService.createSale(params);
   res.json(response.result).status(response.statusCode);
 });
--- services/api/src/modules/services/SalesService.ts
+++ services/api/src/modules/services/SalesService.ts
@@ -4,7 +4,7 @@ import { jsonResponse, type JsonResponseMsg } from "../utils/Response.js";
 import { inject, injectable } from "inversify";
 import { StatusCodes } from "http-status-codes";
 import { SalesTable } from "../../db/schema.js";
-import { getTableColumns } from "drizzle-orm";
+import { asc, getTableColumns } from "drizzle-orm";
 
 export type FlashSaleParams = {
   startTime: Date;
@@ -16,25 +16,48 @@ export class SalesService {
   @inject(TOKENS.DB)
   private db!: Database;
 
-  async getSaleStatus(): Promise<JsonResponseMsg> {
+  async getAllSales(): Promise<JsonResponseMsg> {
     try {
       const { id, ...returnedColumns } = getTableColumns(SalesTable);
-      const sales = await this.db.select(returnedColumns).from(SalesTable);
+      const sales = await this.db.select(returnedColumns)
+        .from(SalesTable) 
+        .orderBy(asc(SalesTable.startTime))
       return jsonResponse({
         statusCode: StatusCodes.OK,
         data: sales,
-        message: "Successfully retrieve sale status!",
+        message: "Successfully retrieve all sales!",
       });
     } catch (error: unknown) {
       return jsonResponse<null>({
         statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-        message: "Something went wrong while fetching sale status.",
+        message: "Something went wrong while fetching all sales.",
         data: null,
       })
     }
   }
 
-  async createFlashSale(params: typeof SalesTable.$inferInsert): Promise<JsonResponseMsg> {
+  async getLatestSale(): Promise<JsonResponseMsg> {
+    try {
+      const { id, ...returnedColumns } = getTableColumns(SalesTable);
+      const [sale] = await this.db.select(returnedColumns)
+        .from(SalesTable) 
+        .orderBy(asc(SalesTable.startTime))
+        .limit(1);
+      return jsonResponse({
+        statusCode: StatusCodes.OK,
+        data: sale,
+        message: "Successfully retrieve latest sale!",
+      });
+    } catch (error: unknown) {
+      return jsonResponse<null>({
+        statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+        message: "Something went wrong while fetching latest sale.",
+        data: null,
+      })
+    }
+  }
+
+  async createSale(params: typeof SalesTable.$inferInsert): Promise<JsonResponseMsg> {
     try {
       if (params.startTime == null || params.endTime == null) return jsonResponse({
         statusCode: StatusCodes.BAD_REQUEST,
@@ -44,21 +67,21 @@ export class SalesService {
 
       const { id, ...returnedColumns } = getTableColumns(SalesTable);
 
-      const sales = await this.db.insert(SalesTable).values({
+      const [sale] = await this.db.insert(SalesTable).values({
         startTime: new Date(params.startTime),
         endTime: new Date(params.endTime)
       }).returning(returnedColumns);
 
       return jsonResponse({
         statusCode: StatusCodes.CREATED,
-        data: sales,
-        message: "Successfully created sale status!",
+        data: sale,
+        message: "Successfully created new sale!",
       });
     } catch (error: unknown) {
       console.log(error);
       return jsonResponse<null>({
         statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-        message: "Something went wrong while creating sale status.",
+        message: "Something went wrong while creating new sale.",
         data: null,
       })
     }
