abd2d867122264dc28701376d4b01f57c410c13f
Geo Galagaran <17492631+geeofree@users.noreply.github.com>
2026-09-19 09:12:26 +0800

    feat: create CRUD endpoints for products

--- services/api/src/modules/routers/ProductsRouter.ts
+++ services/api/src/modules/routers/ProductsRouter.ts
@@ -1,20 +1,27 @@
 import { Router } from "express";
 import { DIContainer } from "../utils/DependencyInjection.js";
-import { RedisService } from "../services/RedisService.js";
-import type { Redis } from "ioredis";
 import { ProductsService } from "../services/ProductsService.js";
 
 export const ProductsRouter = Router();
 
 ProductsRouter.get('/', async (_req, res) => {
-  const redis = DIContainer.get(RedisService) as Redis;
-  await redis.set('products', 'hi products');
-  const products = await redis.get('products');
-  res.json({ products });
+  const productsService = DIContainer.get(ProductsService);
+  const response = await productsService.getAllProducts();
+  res.json(response.result).status(response.statusCode);
 });
 
-ProductsRouter.get('/:sku', (_req, res) => {
-  res.json({ product: {} });
+ProductsRouter.post('/', async (req, res) => {
+  const params = req.body;
+  const productsService = DIContainer.get(ProductsService);
+  const response = await productsService.createProduct(params);
+  res.json(response.result).status(response.statusCode);
+});
+
+ProductsRouter.get('/:sku', async (req, res) => {
+  const { sku } = req.params;
+  const productsService = DIContainer.get(ProductsService);
+  const response = await productsService.getProductBySku(Number(sku));
+  res.json(response.result).status(response.statusCode);
 });
 
 ProductsRouter.post('/:sku/purchase', async (_req, res) => {
--- services/api/src/modules/services/ProductsService.ts
+++ services/api/src/modules/services/ProductsService.ts
@@ -1,12 +1,89 @@
 import type { ChannelModel } from "amqplib";
 import { DIContainer, TOKENS } from "../utils/DependencyInjection.js";
-import { injectable } from "inversify";
+import { inject, injectable } from "inversify";
 import { jsonResponse } from "../utils/Response.js";
 import { StatusCodes } from "http-status-codes";
+import { ProductsTable } from "../../db/schema.js";
+import type { Database } from "./DbService.js";
+import { eq, getTableColumns } from "drizzle-orm";
 
 @injectable()
 export class ProductsService {
   private mqConnection = DIContainer.getAsync<ChannelModel>(TOKENS.MQ);
+
+  @inject(TOKENS.DB)
+  private db!: Database;
+
+  async getAllProducts() {
+    try {
+      const products = await this.db
+        .select()
+        .from(ProductsTable);
+      return jsonResponse({
+        statusCode: StatusCodes.OK,
+        data: products,
+        message: "Successfully retrieved all list of products!",
+      });
+    } catch (error) {
+      console.error(error);
+      return jsonResponse<null>({
+        statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+        message: "Something went wrong while retrieving products.",
+        data: null,
+      })
+    }
+  }
+
+  async getProductBySku(sku: number) {
+    try {
+      const [product] = await this.db
+        .select()
+        .from(ProductsTable)
+        .where(eq(ProductsTable.id, sku))
+
+      if (product == null) {
+        return jsonResponse({
+          statusCode: StatusCodes.NOT_FOUND,
+          data: null,
+          message: "Product does not exists.",
+        });
+      }
+
+      return jsonResponse({
+        statusCode: StatusCodes.OK,
+        data: product,
+        message: "Successfully retrieved product!",
+      });
+    } catch (error) {
+      console.error(error);
+      return jsonResponse<null>({
+        statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+        message: "Something went wrong while retrieving the product.",
+        data: null,
+      })
+    }
+  }
+
+  async createProduct(params: typeof ProductsTable.$inferInsert) {
+    try {
+      const allColumns = getTableColumns(ProductsTable);
+      const [newProduct] = await this.db.insert(ProductsTable)
+        .values(params)
+        .returning(allColumns);
+      return jsonResponse({
+        statusCode: StatusCodes.CREATED,
+        data: newProduct,
+        message: "Successfully created new product!",
+      });
+    } catch (error) {
+      console.error(error);
+      return jsonResponse<null>({
+        statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+        message: "Something went wrong while creating the product.",
+        data: null,
+      })
+    }
+  }
 
   async createOrder() {
     const queueKey = 'orders';
